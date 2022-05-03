597be04443355f6ed67d3790c3615d4ade21965b
hono3bono3 <97086033+hono3bono3@users.noreply.github.com>
2022-05-03 16:31:50 +0900

    App.js修正

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          自動でデプロイされるで！
         </a>
       </header>
     </div>
